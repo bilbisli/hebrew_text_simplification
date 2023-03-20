30bc5dd2bc27f2e9ade99037375f5502cdebb59f
Ariel Eps <butafliko@gmail.com>
2023-03-20 12:20:58 +0200

    adding style and change popup widow

--- SimplificationExtension/popup.js
+++ SimplificationExtension/popup.js
@@ -5,16 +5,34 @@ $(function () {
     if (search_topic) {
       chrome.runtime.sendMessage({ topic: search_topic }, function (response) {
         result = response.farewell;
-        alert(result.summary);
 
-        var notifOptions = {
-          type: "basic",
-          iconUrl: "icon48.png",
-          title: "Simplification For Your Result",
-          message: result.summary,
-        };
+        var html =
+          "<div id='simplified-text' dir='rtl'>" +
+          "<h3>Simplified Text</h3>" +
+          "<p class='result'>" +
+          result.summary +
+          "</p>" +
+          "</div>";
 
-        chrome.notifications.create("Simplification", notifOptions);
+        var popupWindow = window.open(
+          "",
+          "Simplified Text",
+          "width=400,height=300"
+        );
+        popupWindow.document.write(html);
+
+        var style = popupWindow.document.createElement("style");
+        style.innerHTML =
+          "#simplified-text { padding: 20px; background-color: #ffffff; border: 1px solid #cccccc; border-radius: 5px; box-shadow: 0 0 5px #cccccc; text-align: center; font-size: 1.2em; line-height: 1.5em; } h3 { margin: 0; font-size: 1.5em; font-weight: bold; } .result { margin-top: 10px; padding: 10px 0; } button { display: block; margin: 20px auto 0; background-color: #5cb85c; border: none; border-radius: 5px; color: #ffffff; font-size: 1.2em; font-weight: bold; padding: 10px 20px; cursor: pointer; } button:hover { background-color: #4cae4c; }";
+        popupWindow.document.head.appendChild(style);
+
+        var closeButton = popupWindow.document.createElement("button");
+        closeButton.innerHTML = "Close";
+        closeButton.addEventListener("click", function () {
+          popupWindow.close();
+        });
+
+        popupWindow.document.body.appendChild(closeButton);
       });
     }
 
