4b54bac5407df85157adc9ac7be7c0a5aff44a58
Ariel Eps <butafliko@gmail.com>
2023-05-08 22:31:11 +0300

    change font to 25px, fixed the position of the tooltip and tidied the code

--- SimplificationExtension/actions.js
+++ SimplificationExtension/actions.js
@@ -1,66 +1,14 @@
-var jQuery = window.$;
+let port = 8000;
+let ip = "http://127.0.0.1";
+let microSeviceSubPath = "get_simplified/?text=";
 
-// using jQuery
-function getCookie(name) {
-  var cookieValue = null;
-  if (document.cookie && document.cookie != "") {
-    var cookies = document.cookie.split(";");
-    for (var i = 0; i < cookies.length; i++) {
-      var cookie = jQuery.trim(cookies[i]);
-      // Does this cookie string begin with the name we want?
-      if (cookie.substring(0, name.length + 1) == name + "=") {
-        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-        break;
-      }
-    }
-  }
-  return cookieValue;
-}
-
-function csrfSafeMethod(method) {
-  // these HTTP methods do not require CSRF protection
-  return /^(GET|HEAD|OPTIONS|TRACE)$/.test(method);
-}
-
-var csrftoken = getCookie("csrftoken");
-
-$.ajaxSetup({
-  beforeSend: function (xhr, settings) {
-    if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-      xhr.setRequestHeader("X-CSRFToken", csrftoken);
-    }
-  },
-});
-
-var getSelected = function () {
-  var t = "";
-  if (window.getSelection) {
-    t = window.getSelection();
-  } else if (document.getSelection) {
-    t = document.getSelection();
-  } else if (document.selection) {
-    t = document.selection.createRange().text;
-  }
-  if (t != "") {
-    var getRange = t.getRangeAt(0);
-    return [
-      t.toString().trim().toLowerCase(),
-      getRange.getBoundingClientRect(),
-      t,
-    ]; // 0: selected text, 1: location in document, 2: all text
-  }
-  return [null, null];
-};
-
-class PopupTooltip {
-  constructor(range, template_html = null) {
-    this.range = range;
-    this.contentHeight = 100;
-    if (template_html == null) {
-      this.loadTemplate(template_html);
-    } else {
-      this.popupHtml = template_html;
-    }
+class Tooltip {
+  constructor() {
+    this.tooltipElement = null;
+    this.selectedText = "";
+    this.fontSize = 25;
+    this.textId = "tooltipp-text-content";
+    this.contentId = "tooltipp-content";
   }
 
   createTitle(title_text) {
@@ -68,123 +16,98 @@ class PopupTooltip {
     title_html.setAttribute("class", "sr_title");
     let strong_div = document.createElement("strong");
     strong_div.setAttribute("id", "tooltipp-title-b");
-    strong_div.setAttribute("style", "font-size: 17px");
-    this.contentHeight += Math.ceil(title_text.length / 86.0) * 12;
+    strong_div.setAttribute("style", `font-size: ${this.fontSize}px`);
     strong_div.textContent = title_text;
     title_html.appendChild(strong_div);
-    return title_html.outerHTML;
+    return title_html;
   }
 
   createText(content_text, text_class = "e") {
     let text_html = document.createElement("div");
     text_html.setAttribute("class", "sr_" + text_class);
-    text_html.setAttribute("id", "tooltipp-text-content");
-    text_html.setAttribute("style", "font-size: 17px");
-    this.contentHeight += Math.ceil(content_text.length / 86.0) * 12;
+    text_html.setAttribute("id", this.textId);
+    text_html.setAttribute("style", `font-size: ${this.fontSize}px`);
     text_html.textContent = content_text;
-    return text_html.outerHTML;
+    return text_html;
   }
 
-  addSection(snippet) {
-    this.popupHtml = snippet + this.popupHtml;
+  create() {
+    this.tooltipElement = document.createElement("div");
+    this.tooltipElement.classList.add("tooltipp");
+
+    let tooltipContent = document.createElement("div");
+    tooltipContent.classList.add(this.contentId);
+
+    let tooltipText = document.createElement("div");
+    tooltipText.classList.add("tooltipp-text");
+
+    let title = this.createTitle("טקסט מפושט");
+    console.log(title.offsetHeight);
+    tooltipText.appendChild(title);
+    tooltipText.appendChild(this.createText(this.selectedText));
+    tooltipContent.appendChild(tooltipText);
+
+    tooltipContent.style.fontSize = `${this.fontSize}px`;
+    this.tooltipElement.appendChild(tooltipContent);
+
+    this.tooltipElement.style.zIndex = "9999"; // Adjust the z-index value as needed
+    this.tooltipElement.style.pointerEvents = "none"; // Allow events to pass through
+    document.body.appendChild(this.tooltipElement);
   }
 
-  addTextSection(titleText, contentText) {
-    let titleHtml = this.createTitle(titleText);
-    let contetTextHtml = this.createText(contentText);
-    let textSection = titleHtml + contetTextHtml;
-    this.addSection(textSection);
-  }
+  show(selectedText, position) {
+    this.selectedText = selectedText;
+    let textElement = document.getElementById(this.textId);
+    let contentElement = document.getElementById(this.contentId);
+    textElement.innerHTML = this.modifyText(this.selectedText);
 
-  loadTemplate(template_html) {
-    // const template = document.createElement("tooltip");
-    // var ajax = new XMLHttpRequest();
+    const range = window.getSelection().getRangeAt(0);
+    const rect = range.getBoundingClientRect();
+    const scrollY = window.pageYOffset || document.documentElement.scrollTop;
 
-    // ajax.open("GET", template_html, false);
-    // ajax.send();
-    // template.innerHTML += ajax.responseText;
-    // this.popupHtml = template.content;
-    this.popupHtml = "";
-  }
+    this.tooltipElement.style.left = `${rect.left}px`;
+    this.tooltipElement.style.width = `${rect.width}px`; // Set width to match selected text
+    this.tooltipElement.style.display = "block";
 
-  reset() {
-    this.popupHtml = "";
-  }
-
-  getPosition() {
-    let x = this.range.left + this.range.width / 2 + 200;
-    let y = this.range.currentY - this.range.height;
-    if (x > window.innerWidth) {
-      x = window.innerWidth;
+    if (position === "below") {
+      console.log("below");
+      let tooltipHeight = textElement.offsetHeight + 120;
+      this.tooltipElement.style.top = `${
+        rect.top + scrollY - tooltipHeight - 5
+      }px`;
+    } else {
+      console.log("above");
+      console.log(this.tooltipElement.offsetHeight);
+      this.tooltipElement.style.top = `${rect.bottom + scrollY + 5}px`;
     }
-    if (y - this.range.height < 400) {
-      y = this.range.bottom + this.contentHeight + 90;
-    }
-    return { x: x, y: y };
+  }
+  hide() {
+    this.tooltipElement.style.display = "none";
+    this.selectedText = "";
   }
 
-  getHtml(xPos = null, yPos = null) {
-    if (xPos == null || yPos == null) {
-      let pos = this.getPosition();
-      xPos = pos.x;
-      yPos = pos.y;
-    }
-    let output =
-      '<div class="tooltipp" style="top: ' +
-      yPos +
-      "px !important; left: " +
-      xPos +
-      'px !important;">';
-    output += '<span class="tooltipp-content">';
-    output += '<span class="tooltipp-text">';
-    output += this.popupHtml;
-    output += "</span>";
-    output += "</span>";
-    output += "</div>";
-
-    return output;
+  modifyText(text) {
+    // Modify the selected text here according to your requirements
+    return text.toUpperCase();
   }
 }
-let port = 8000;
-let ip = "http://127.0.0.1";
-let microSeviceSubPath = "get_simplified/?text=";
 
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("Checkbox value is:", checkboxValue);
-});
+const tooltip = new Tooltip();
+tooltip.create();
+let simpleText = "";
 
-$(document).ready(function () {
+document.addEventListener("mouseup", function (event) {
+  const selectedText = window.getSelection().toString();
   chrome.storage.sync.get("checkboxValue", function (data) {
     const checkboxValue = data.checkboxValue;
-    console.log("Checkbox value is:", checkboxValue);
-
-    console.log("Checkbox value is:", checkboxValue);
-    $("body").on("click", function (e) {
-      var container = $(".tooltipp");
-
-      if (!container.is(e.target) && container.has(e.target).length === 0) {
-        $("#tooltip").remove();
-        $(".tooltipp").remove();
-      }
-    });
-
-    $("body").on("click", function (e) {
-      console.log("click2");
-      const selected = getSelected();
-      console.log("selected: " + selected);
-      let TextElement = selected[2];
+    if (selectedText.length > 0) {
+      let TextElement = selectedText;
       console.log("TextElement: " + TextElement);
       if (TextElement) {
-        let selectedRange = selected[1];
-        selectedRange.currentY = e.pageY;
-        console.log(selectedRange);
-        let st = selected[0];
-
-        if (st != null && st.length != 0) {
-          if (e.target.id !== "checkbox") {
-            // check if the event target is not the checkbox
-            let toolTip = new PopupTooltip(selectedRange);
-            chrome.runtime.sendMessage({ text: st }, async function (response) {
+        if (event.target.id !== "checkbox") {
+          chrome.runtime.sendMessage(
+            { text: TextElement },
+            async function (response) {
               console.log("msg???");
               console.log("response: " + response);
               if (checkboxValue == false) {
@@ -195,44 +118,26 @@ $(document).ready(function () {
               console.log(checkboxValue);
               console.log("ress???");
               console.log("result: " + simpleText);
-              toolTip.addTextSection("טקסט מפושט:", simpleText);
-              console.log(toolTip.getHtml());
-              ttHtml = toolTip.getHtml();
-              $("body").append(ttHtml);
-              toolTip.reset();
-            }); // pass checkboxValue as a parameter
-          }
+
+              const rect = window
+                .getSelection()
+                .getRangeAt(0)
+                .getBoundingClientRect();
+              const position =
+                rect.bottom > window.innerHeight / 2 ? "below" : "above";
+
+              tooltip.show(simpleText, position);
+            }
+          ); // pass checkboxValue as a parameter
         }
       }
-    });
+    }
   });
 });
 
-function stringToUint(string) {
-  var string = btoa(unescape(encodeURIComponent(string))),
-    charList = string.split(""),
-    uintArray = [];
-  for (var i = 0; i < charList.length; i++) {
-    uintArray.push(charList[i].charCodeAt(0));
-  }
-  return new Uint8Array(uintArray);
-}
-
-function getWords() {
-  var collectedText;
-
-  $("p,h1,h2,h3,h4,h5").each(function (index, element) {
-    collectedText += element.innerText + "\n";
-  });
-
-  collectedText = collectedText.replace("undefined", "");
-
-  collectedText = collectedText.replace(/[0-9]/g, "");
-
-  console.log(collectedText);
-}
-
-//////////////////////////////
+document.addEventListener("mousedown", function (event) {
+  tooltip.hide();
+});
 
 chrome.runtime.connect().onDisconnect.addListener(function () {
   // clean up when content script gets disconnected
